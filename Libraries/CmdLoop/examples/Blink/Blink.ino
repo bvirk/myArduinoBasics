1e9ff1ccb4345868f3d8cf3486e4ed5e02252623
Benny Andersen <a.mail.user@gmail.com>
2020-05-15 17:12:32 -0100

    blink usage focused

--- Libraries/CmdLoop/examples/Blink/Blink.ino
+++ Libraries/CmdLoop/examples/Blink/Blink.ino
@@ -2,8 +2,8 @@
 #include <Sendf.h>
 
 namespace slices {
-	uint16_t onTime=2100;
-	uint16_t offTime=100;
+	uint16_t onTime=2000; 
+	uint16_t offTime=300;
 	void goingOn(CmdLoop & cmdLoop) {
 		digitalWrite(13,1);
 		cmdLoop.setDelay(onTime);
@@ -25,48 +25,30 @@ namespace cmdFuncs {
 	/** 
 	  * Very version of this precious mechanism 
 	  */
-	uint8_t  ver(int argc, char* argv[]) {
-		sendf("command loop v0.1\n");
+	int8_t  ver(int argc, char* argv[]) {
+		sendf(F("command loop v0.1\n"));
 		return 0;
 	}
-	
-	/** 
-	  * Like hello world style demo of arguments to main
-	  */
-	uint8_t printArgs(int argc, char* argv[]) {
-		for (uint8_t i = 0; i < argc; i++)
-			sendf("%d: >%s<\n",i,argv[i]);
-	}
 
-	uint8_t blink(int argc, char* argv[]) {
+	int8_t blink(int argc, char* argv[]) {
 		if (argc == 3 ) {
 			slices::onTime = atoi(argv[1]);
 			slices::offTime = atoi(argv[2]);
-		} else
-			sendf("syntax: blink ontime offtime\n");
+			return 0;
+		} 
+		return -1;
 	}
 	
-	/** 
-	  * Whole namespace of functions 
-	  */
-	uint8_t  help(int argc, char *argv[]);
-
 	
 	/** 
 	  * Array of names to functions 
 	  */
 	  CommandFunc commands[] = {
-		{"ver",ver}
-		,{"printargs",printArgs}
-		,{"blink",blink}
-		,{"help",help}
+		 {blink,"blink"}
+		,{ver,"ver"}
+		,{ver,"ver"} // or link error???
 	};
   
-  uint8_t  help(int argc, char *argv[]) {
-  	  sendf("Available commands\n");
-  	  for (int i=0; i < sizeof(commands)/sizeof(commands[0]); i++)
-  	  	  sendf("%s\n",commands[i].name);
-  }
 };
 
 CmdLoop cmdLoop( 
@@ -75,10 +57,9 @@ CmdLoop cmdLoop(
 	);
 
 void setup() {
-	Serial.begin(9600);
-	pinMode(13,OUTPUT);
-	sendf("command loop v0.2\n");
-	sendf("command: help for viewing commands\n");
+	Serial.begin(115200);
+	pinMode(13,1);
+	sendf(F("syntax: blink ontime offtime"));
 	cmdLoop.loop();
 }
 
