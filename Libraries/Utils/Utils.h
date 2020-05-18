f8ff1e4b236c12b40bfe7173ed82c28c46dffdb3
Benny Andersen <a.mail.user@gmail.com>
2020-05-18 14:23:27 -0100

    documentation

--- Libraries/Utils/Utils.h
+++ Libraries/Utils/Utils.h
@@ -2,7 +2,17 @@
 #define Utils_h
 #include <stdint-gcc.h>
 namespace Utils {
-	void hexdump(const void *p, uint16_t size);
-	void eepromDump(uint16_t start, uint16_t length);
+	/*! Hexidecimal bytewide dump of ram memory
+	  \param p point to lowest memory address which content is showed
+	  \param size is length of memory be showed
+	  */
+    void hexdump(const void *p, uint16_t size);
+
+	
+    /*! Hexidecimal bytewide dump of EEPROM memory
+	  \param start is lowest memory address which content is showed
+	  \param lenght is size of memory be showed
+	  */
+    void eepromDump(uint16_t start, uint16_t length);
 };
 #endif
