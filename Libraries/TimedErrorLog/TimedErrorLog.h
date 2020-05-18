3c7444deb961fa7d6e7bae3666b1dd791917b259
Benny Andersen <a.mail.user@gmail.com>
2020-05-18 16:16:03 -0100

    doc added

--- Libraries/TimedErrorLog/TimedErrorLog.h
+++ Libraries/TimedErrorLog/TimedErrorLog.h
@@ -3,21 +3,53 @@
 #include <stdint-gcc.h>
 
 #define TIMED_ERROR_LOG_ADDRESS 0
+
+//! Byond reset, one item error log.
+/*!  
+  The saved time denoted error consisting of a single 8 bit number.
+  The used 11 bytes EEPROM memory is the real object reestablished by default construction. An error number can only be set one
+  time and survives reset. To enable being able to recieve an error number, a constuction with parameter time must be
+  (re)performed. A device on which a TimeErrorLog newer has been constructed will on default construction have bogus settings.
+  */
 class TimedErrorLog {
-	uint32_t secSReset : 24; 	//! seconds sinse reset.
-	uint32_t dateTime; 			//! seconds =0, yymmddhhmm of millisOfTime
-	int8_t errNr;				//! setted by setError and cleared by construction with parameter timestamp
-	uint32_t secAtError: 24;	//! seconds adjustment to secSReset that indicates time of error.
+	uint32_t secSReset : 24; 	//!< seconds sinse reset.
+	uint32_t dateTime; 			//!< seconds =0, yymmddhhmm of millisOfTime
+	int8_t errNr;				//!< setted by setError and cleared by construction with parameter timestamp
+	uint32_t secAtError: 24;	//!< seconds adjustment to secSReset that indicates time of error.
 	
 	uint32_t secOfYear(uint8_t yy, uint8_t mo, uint8_t dd, uint8_t hh, uint8_t mi, uint8_t *pMonlen);
   	enum twoDecimalDigits {SS=0,NN,HH,DD,MM,YY};
 
 public: 	
-  	TimedErrorLog();
+  	//! Reestablish that object EEPROM contains  
+    TimedErrorLog();
+    
+    
+    //! Construct an object from a timestamp
+    /*!
+      \param timestamp is a ten digit integer with format yymmddhhnn (n=mi_n_uts)
+      */
   	TimedErrorLog(uint32_t timestamp);
+  	
+  	
   	enum showmode {SETTEDTIME,NOW,ERROR};
+  	
+  	
+  	//! Report serially time or error number
+  	/*!
+  	  \param show is one off
+  	  - SETTEDTIME 
+  	  - NOW
+  	  - ERROR
+  	  and reflects respectively time of construction, current time and time of logged error
+  	  */
   	bool show(showmode show);
+  	
+  	
+  	//! Set the error number
   	void setError(int errorNr);
+  	
+  	//! Get the error number.
   	int8_t getError(); 
 };
 
