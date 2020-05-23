d287f1c722f5cb99fcdba14c387a3fcb5d77c058
Benny Andersen <a.mail.user@gmail.com>
2020-05-23 15:40:25 -0100

    license and version standard

--- Libraries/Utils/Utils.h
+++ Libraries/Utils/Utils.h
@@ -1,5 +1,12 @@
 #ifndef Utils_h
 #define Utils_h
+// This library is free software; you can redistribute it and/or 
+// modify it under the terms of the GNU Lesser General Public 
+// License as published by the Free Software Foundation; either 
+// version 2.1 of the License, or (at your option) any later version.
+
+#define UTILSLIBVERSION "0.0.1"
+
 #include <stdint-gcc.h>
 #include <avr/pgmspace.h>
 
