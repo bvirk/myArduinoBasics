7267255313266a0219b62e3e270e359cd3f3f51f
Benny Andersen <a.mail.user@gmail.com>
2020-05-25 09:44:14 -0100

    flash from define

--- Libraries/Utils/examples/Hexdump/Hexdump.ino
+++ Libraries/Utils/examples/Hexdump/Hexdump.ino
@@ -1,12 +1,14 @@
 #include "Utils.h"
 #include "Sendf.h"
 
+#define SHOWMYRAMUSAGE "Shakespeare's total work would fill flash memory"
+void * dummy(__FlashStringHelper * p) { return p;}
 
 void setup() {
 	Serial.begin(115200);
 	utils::dataHexdump("Hello world",12);
 	sendf("\n");
-	utils::pgmHexdump(F("flash is great"),15);
+	utils::pgmHexdump(dummy(F(SHOWMYRAMUSAGE)),49);
 	sendf("\n");
 	utils::eepromHexdump(0,64);
 }
